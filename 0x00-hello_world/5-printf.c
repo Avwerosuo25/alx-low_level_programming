01f4acd62704b434a5e5a66665d6b6fe844cf5dd
Avwerosuo25 <saduwakelvin425@gmail.com>
2022-10-14 02:59:36 -0700

    Script to print a string with command printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -entry point
+ * description - C program that prints a function followed by a new line
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
