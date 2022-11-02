2bbd06261c01a07e52c4cd3f0536075eaec5834e
Avwerosuo25 <saduwakelvin425@gmail.com>
2022-11-01 21:56:14 -0700

    additional task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string -this sets the value of the pointer to a character
+ * @s: The pointer.
+ * @to: The char.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,21 +10,21 @@
 
 void print_diagsums(int *a, int size)
 {
-	int i = 0;
-	int j = size - 1;
+	int x = 0;
+	int y = size - 1;
 	int sum1 = 0;
 	int sum2 = 0;
 
-	while (i <= (size * size))
+	while (x <= (size * size))
 	{
-		sum1 = sum1 + a[i];
-		i = i + size + 1;
+		sum1 = sum1 + a[x];
+		x = x + size + 1;
 	}
 
-	while (j < (size * size - 1))
+	while (y < (size * size - 1))
 	{
-		sum2 += a[j];
-		j = j + size - 1;
+		sum2 += a[y];
+		y = y + size - 1;
 	}
 
 	printf("%d, %d\n", sum1, sum2);
