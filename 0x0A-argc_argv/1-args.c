7d635eb13b6a8ca6370553696bab138f2197857b
Avwerosuo25 <saduwakelvin425@gmail.com>
2022-11-06 03:31:47 -0800

    Updated

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,16 +10,12 @@
 
 int main(int argc, char *argv[])
 {
-	int i;
+	int a;
+	(void)argv;
 
-	if (argc == 1)
-		printf("%d\n", argc - 1);
-	else
-	{
-		for (i = 0; *argv; i++, argv++);
-
-		printf("%d\n", i - 1);
-	}
+	for (a = 0; a <= argc; a++)
+	{}
+	printf("%d\n", a - 2);
 
 	return (0);
 }
