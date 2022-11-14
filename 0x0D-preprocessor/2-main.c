597c8d57d6c6ee431f9f65e1164e99b843a5b5f4
Avwerosuo25 <saduwakelvin425@gmail.com>
2022-11-14 14:19:03 -0800

    A file that prints the name of the file that compiled it followed by a new linw

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * file - 2-main.c
+ * Description: file that prints the file it was
+ * compiled from, followed by a new line
+ * Return: 0 
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
