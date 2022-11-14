be249addf6f08b053b3846fe94aaab811e6ae19f
Avwerosuo25 <saduwakelvin425@gmail.com>
2022-11-14 14:35:25 -0800

    Macro to sum numbers

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * file - 4-sum.h
+ * Description: header file that defines macro SUM,
+ * to compute sum of two numbers
+ */
+
+#define SUM(x, y) (x) + (y)
+
+#endif
