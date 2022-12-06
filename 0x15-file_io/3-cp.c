5508b77b210697cea1f965df8d02b355ae3b22ac
Avwerosuo25 <kelvinklein425@gmail.com>
2022-12-06 01:07:44 -0800

    a program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -72,13 +72,13 @@ int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
-	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-	exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 	if (argv[1] == NULL)
 	{
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-	exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 	cp(argv[1], argv[2]);
 	return (0);
